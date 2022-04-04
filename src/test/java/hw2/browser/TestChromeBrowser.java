5d71a9a9f654dde0abc362295f026e79d0c412c8
90101_Vereshchagina-Sofia <vereshagina.sofya@yandex.ru>
2022-04-04 23:28:50 +0300

    Homework 2 is completed.

---
+++ src/test/java/hw2/browser/TestChromeBrowser.java
@@ -0,0 +1,53 @@
+package hw2.browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestChromeBrowser {
+    protected WebDriver driver;
+    protected SoftAssert softAssert;
+    protected String homeUrl;
+
+    private String login = "Roman";
+    private String password = "Jdi1234";
+    private String name = "ROMAN IOVLEV";
+
+    @BeforeMethod
+    public void startBrowser() {
+        //настройка драйвера && браузера
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        softAssert = new SoftAssert();
+        homeUrl = "https://jdi-testing.github.io/jdi-light/index.html";
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+    }
+
+    //отдельный тест на запуск браузера и авторизацию
+    @Test
+    public void logIn() {
+        //проверка страницы
+        driver.get(homeUrl);
+        softAssert.assertEquals(driver.getTitle(), "Home Page");
+        driver.findElement(By.cssSelector(".uui-profile-menu .dropdown-toggle")).click();
+        //авторизация
+        driver.findElement(By.id("name")).sendKeys(login);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+        //проверка user a
+        WebElement userName = driver.findElement(By.id("user-name"));
+        softAssert.assertEquals(userName.getText(), name);
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        //выход из браузера
+        driver.quit();
+    }
+}
---
+++ src/test/java/hw2/ex2/ChromeTest2.java
@@ -0,0 +1,60 @@
+package hw2.ex2;
+
+import hw2.browser.TestChromeBrowser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ChromeTest2 extends TestChromeBrowser {
+    @Test
+    public void chromeTest2() {
+        //1-4
+        logIn();
+
+        //5.1
+        WebElement service = driver.findElement(By.cssSelector("header .nav .dropdown"));
+        service.click();
+
+        //5.2
+        WebElement diffElements = service.findElement(By.xpath("//*[text()='Different elements']"));
+        diffElements.click();
+
+        //6
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("label.label-checkbox"));
+        checkBoxes.removeIf(x -> !x.getText().equals("Water") && !x.getText().equals("Wind"));
+        softAssert.assertEquals(checkBoxes.size(), 2);
+        for (WebElement box : checkBoxes) {
+            box.findElement(By.tagName("input")).click();
+        }
+
+        //7
+        List<WebElement> radios = driver.findElements(By.cssSelector("label.label-radio"));
+        radios.removeIf(x -> !x.getText().equals("Selen"));
+        softAssert.assertEquals(radios.size(), 1);
+        for (WebElement button : radios) {
+            button.findElement(By.tagName("input")).click();
+        }
+
+        //8
+        WebElement yellow = driver.findElement(By.xpath("//select/option[text()='Yellow']"));
+        yellow.click();
+
+        //9
+        String[] expectedLogs = {"Colors: value changed to Yellow", "metal: value changed to Selen", "Wind: condition changed to true", "Water: condition changed to true"
+        };
+
+        List<WebElement> log = driver.findElements(By.cssSelector(".info-panel-body-log li"));
+
+        softAssert.assertEquals(log.size(), expectedLogs.length);
+        for (int i = 0; i < log.size() && i < expectedLogs.length; i++) {
+            WebElement current = log.get(i);
+            softAssert.assertTrue(current.isDisplayed());
+            softAssert.assertTrue(current.getText().contains(expectedLogs[i]));
+        }
+        softAssert.assertAll();
+        
+        //10 in parent class in after method
+    }
+}
